a9858b8e9e26c67aa94a4dc6767c83b3146a902f
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-06-15 19:41:03 -0300

    Chore: adding logic

--- src-tauri/src/features/filesystem.rs
+++
@@ -1,3 +0,0 @@
-use crate::features::functions::mount_point::get_mount_point;
-
-pub fn test() {}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,17 +2,55 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod utils;
-mod features;
+mod filesystem;
+mod search;
+mod errors;
 
 use tauri::Menu;
-use utils::theme::{ change_theme, read_theme };
-use utils::splashscreen::close_splashscreen;
+use std::sync::{ Arc, Mutex };
+use std::collections::HashMap;
+use serde::{ Deserialize, Serialize };
 
-fn main() {
+use search::search_directory;
+use filesystem::volume::get_volumes;
+use utils::splashscreen::close_splashscreen;
+use utils::theme::{ change_theme, read_theme };
+use filesystem::explorer::{ open_file, open_directory, create_file, create_directory, rename_file, delete_file };
+
+#[derive(Serialize, Deserialize)]
+pub struct CachedPath {
+    #[serde(rename = "p")]
+    file_path: String,
+    #[serde(rename = "t")]
+    file_type: String,
+}
+
+pub type VolumeCache = HashMap<String, Vec<CachedPath>>;
+
+#[derive(Default)]
+pub struct AppState {
+    system_cache: HashMap<String, VolumeCache>,
+}
+
+pub type StateSafe = Arc<Mutex<AppState>>;
+
+#[tokio::main]
+async fn main() {
   let menu = Menu::new();
 
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![close_splashscreen, change_theme, read_theme])
+    .invoke_handler(tauri::generate_handler![
+        close_splashscreen, 
+        change_theme, 
+        read_theme, 
+        open_file, 
+        open_directory, 
+        create_file, 
+        create_directory, 
+        rename_file, delete_file, 
+        search_directory, 
+        get_volumes
+      ])
     .menu(menu)
     .on_menu_event(|event| {
       match event.menu_item_id() {
